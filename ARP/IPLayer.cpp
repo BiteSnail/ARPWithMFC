513ebd8807756ee1a424124f5c2fc3a97bd7ac7d
LLagoon3 <choonarm3@gmail.com>
2022-11-13 16:25:00 +0900

    Mod IPLayer Functions

--- ARP/IPLayer.cpp
+++ ARP/IPLayer.cpp
@@ -2,3 +2,54 @@
 #include "pch.h"
 #include "stdafx.h"
 #include "IPLayer.h"
+
+#define IP__MAX_SIZE = null//??
+#define IP_HEADER_SIZE = null//??
+
+IPLayer::IPLayer(char* pName) : CBaseLayer(pName){
+    ResetHeader();
+}
+
+IPLayer::~IPLayer(){
+}
+
+void IPLayer::ResetHeader(){
+    memset(m_sHeader.ip_data, 0, 6);
+    memset(m_sHeader.ip_dstaddr, 0, 6);
+    memset(m_sHeader.ip_src_addr, IP_MAX_DATA_SIZE, 6);//IP_MAX_DATA_SIZE = IP_MAX_SIZE - IP_HEADER_SIZE
+    //m_sHeader.ip_type = 0;
+}
+
+unsigned char* IPLayer::GetSourceAddress(){
+    return m_sHeader.ip_srcaddr;
+}
+
+unsigned char* IPLayer::GetDestinAddress{
+    return m_sHeader.ip_dstaddr;
+}
+
+void IPLayer::SetSourceAddress(unsigned char* pAddress){
+    memcpy(m_sHeader.ip_srcaddr, pAddress, 6);
+}
+
+void IPLayer::SetDestinAddress(unsigned char* pAddress){
+    memcpy(m_sHeader.ip_dstaddr, pAddress, 6);
+}
+//UpperLayer = AppLayer, UnderLayer = ARPLayer -> GetUnderLayer함수로 작동?
+BOOL IPLayer::Send(unsigned char* ppayload, int nlength){
+    memcpy(m_sHeader.ip_data, ppayload, nlength);
+    BOOL bSuccess = FALSE;
+    bSuccess = this->GetUnderLayer()->Send((unsigned char*)&m_sHeader, IP_HEADER_SIZE + nlength);
+    return bSuccess;
+}
+
+BOOL IPLayer::Receive(unsigned char* ppayload){
+    BOOL bSuccess = FALSE;
+    IP_HEADER pFrame = (IP_HEADER)ppayload;
+
+    if(memcmp(pFrame->ip_dstaddr, m_sHeader.ip_srcaddr, sizeof(m_sHeader.ip_srcaddr)) == 0){
+        bSuccess = mp_aUpperLayer[0]->Receive(pFrame->ip_data)
+    }
+
+    return bSuccess;
+}
--- ARP/IPLayer.h
+++ ARP/IPLayer.h
@@ -1,7 +1,32 @@
 #pragma once
 #include "BaseLayer.h"
 #include "pch.h"
-class IPLayer :
-    public CBaseLayer
-{
+class IPLayer : public CBaseLayer{
+
+private:
+    void ResetHeader();
+
+public:
+
+    BOOL			Receive(unsigned char* ppayload);
+    BOOL			Send(unsigned char* ppayload, int nlength);
+    void			SetDestinAddress(unsigned char* pAddress);
+    void			SetSourceAddress(unsigned char* pAddress);
+    unsigned char*  GetDestinAddress();
+    unsigned char*  GetSourceAddress();
+
+    IPLayer(char* pName);
+	virtual ~IPLayer();
+
+	typedef struct _IP_HEADER {
+        //IP헤더 type 필요??
+		unsigned char	ip_dstaddr[6];
+		unsigned char	ip_srcaddr[6];
+		//unsigned short	type;
+		unsigned char	ip_data[ETHER_MAX_DATA_SIZE];
+
+	} IP_HEADER, *PIP_HEADER;
+
+protected:
+    IP_HEADER m_sHeader
 };
