2ee73cb78d08041f53507fd9e5fbaf14400093e2
동욱 남 <egddbr1213@naver.com>
2022-11-14 17:05:07 +0900

    ARP.rc / ARPDlg.cpp / ARPDlg.h / resource.h 입니다

--- ARP/ARPDlg.cpp
+++ ARP/ARPDlg.cpp
@@ -40,6 +40,8 @@ CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
 void CAboutDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+
+	
 }
 
 BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
@@ -52,19 +54,45 @@ END_MESSAGE_MAP()
 
 CARPDlg::CARPDlg(CWnd* pParent /*=nullptr*/)
 	: CDialogEx(IDD_ARP_DIALOG, pParent)
+	, CBaseLayer("ChatDlg") //
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+
+	//Protocol Layer Setting
+	//m_LayerMgr.AddLayer(new CChatAppLayer("ChatApp"));
+	m_LayerMgr.AddLayer(new CEthernetLayer("Ethernet"));
+	//m_LayerMgr.AddLayer(new FileTransLayer("FileTrans"));
+	m_LayerMgr.AddLayer(new CNILayer("Network"));
+	m_LayerMgr.AddLayer(this);
+
+	// 레이어를 연결한다. (레이어 생성)
+	m_LayerMgr.ConnectLayers("Network ( *Ethernet ( *ChatApp ( *ChatDlg ) *FileTrans ( *ChatDlg ) ) )");
+
+	m_Network = (CNILayer*)m_LayerMgr.GetLayer("Network");
 }
 
 void CARPDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST_CONTROL, m_ctrlListControl);
+	DDX_Control(pDX, IDC_LIST_CONTROL_PROXY, m_ctrlListControlProxy);
+	DDX_Control(pDX, IDC_IPADDRESS, m_IPADDRESS);
+	DDX_Control(pDX, IDC_IPADDRESS2, m_IPADDRESS2);		//<-용도가 정확치 않아 변수명을 이렇게 정했습니다.
+	DDX_Control(pDX, IDC_EDIT_HW_ADDR, m_editHWAddr);
+	DDX_Control(pDX, IDC_EDIT_1, m_edit1);				//<-용도가 정확치 않아 변수명을 이렇게 정했습니다.
+	DDX_Control(pDX, IDC_COMBO1, m_Combox1);		//파워포인트에서 VMware 적힌 부분입니다.
+	
+	
 }
 
 BEGIN_MESSAGE_MAP(CARPDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON_ADD, &CARPDlg::OnBnClickedButtonAdd)
+	ON_BN_CLICKED(IDC_BUTTON_DELETE, &CARPDlg::OnBnClickedButtonDelete)
+	ON_BN_CLICKED(IDC_BUTTON_ITEM_DEL, &CARPDlg::OnBnClickedButtonItemDel)
+	ON_BN_CLICKED(IDC_BUTTON_ALL_DEL, &CARPDlg::OnBnClickedButtonAllDel)
 END_MESSAGE_MAP()
 
 
@@ -99,8 +127,10 @@ BOOL CARPDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
-	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	theApp.MainDlg = (CARPDlg*)AfxGetApp()->m_pMainWnd;
 
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	InitFn();
 	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
 }
 
@@ -153,3 +183,139 @@ HCURSOR CARPDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+
+void CARPDlg::InitFn()
+{
+	m_IPADDRESS.SetWindowTextA(_T("0.0.0.0"));
+	m_IPADDRESS2.SetWindowTextA(_T("0.0.0.0"));
+	m_editHWAddr.SetWindowTextA(_T("00:00:00:00:00:00"));
+	m_edit1.SetWindowTextA(_T("00:00:00:00:00:00"));
+	//--------------------------------------------------------------------------------------
+	// 
+	//
+	//ARP Cache Set
+	//
+	// 
+	//--------------------------------------------------------------------------------------
+	m_ctrlListControl.SetExtendedStyle(LVS_EX_FULLROWSELECT);
+	m_ctrlListControl.InsertColumn(0, _T("IP Address"));
+	m_ctrlListControl.SetColumnWidth(0, 120);
+	m_ctrlListControl.InsertColumn(1, _T("Ethernet Address"));
+	m_ctrlListControl.SetColumnWidth(1, 140);
+	m_ctrlListControl.InsertColumn(2, _T("Status"));
+	m_ctrlListControl.SetColumnWidth(2, 90);
+
+	//--------------------------------------------------------------------------------------
+	// 
+	//
+	//Proxy ARP Cache Set
+	//
+	// 
+	//--------------------------------------------------------------------------------------
+	m_ctrlListControlProxy.SetExtendedStyle(LVS_EX_FULLROWSELECT);
+	m_ctrlListControlProxy.InsertColumn(0, _T("Device"));
+	m_ctrlListControlProxy.SetColumnWidth(0, 70);
+	m_ctrlListControlProxy.InsertColumn(1, _T("IP Address"));
+	m_ctrlListControlProxy.SetColumnWidth(1, 120);
+	m_ctrlListControlProxy.InsertColumn(2, _T("Ethernet Address"));
+	m_ctrlListControlProxy.SetColumnWidth(2, 140);
+	
+	//--------------------------------------------------------------------------------------
+	// 
+	//
+	//
+	//
+	// 
+	//--------------------------------------------------------------------------------------
+	m_Combox1.AddString(_T("VMware Virtual Ethernet Adapter"));
+	m_Combox1.SetCurSel(0);
+	
+	//--------------------------------------------------------------------------------------
+	// 
+	//
+	//Device Add Dlg Set
+	//
+	// 
+	//--------------------------------------------------------------------------------------
+	mDeviceAddDlg.Create(IDD_DIALOG_DEVICE_ADD, this);
+	mDeviceAddDlg.ShowWindow(SW_HIDE);
+	//
+	//
+	// //--------------------------------------------------------------------------------------
+	// 
+	//
+	//테스트용으로 넣은겁니다 지우셔도됩니다.
+	//
+	// 
+	AddArpCache(_T("192.168.011.111"), _T("00:00:00:00:00:00"), _T("InComplete"));
+	AddArpCache(_T("192.168.011.222"), _T("01:00:00:00:00:00"), _T("InComplete"));
+	AddArpCache(_T("192.168.011.333"), _T("02:00:00:00:00:00"), _T("InComplete"));
+	//--------------------------------------------------------------------------------------
+}
+
+
+
+void CARPDlg::AddArpCache(TCHAR* _ip, TCHAR* _Ethernet, TCHAR* _Status)
+{
+	if (_ip == NULL) { return; }
+
+	int nListIndex = m_ctrlListControl.GetItemCount();
+
+	m_ctrlListControl.InsertItem(nListIndex, _ip);
+
+	m_ctrlListControl.SetItemText(nListIndex, 1, _Ethernet);
+	m_ctrlListControl.SetItemText(nListIndex, 2, _Status);
+	m_ctrlListControl.SetColumnWidth(1, LVSCW_AUTOSIZE_USEHEADER);
+	m_ctrlListControl.SendMessage(WM_VSCROLL, SB_BOTTOM);	// Scroll to bottom
+}
+
+void CARPDlg::AddProxyArpCache(TCHAR* _Device, TCHAR* _ip, TCHAR* _Ethernet)
+{
+	if (_ip == NULL) { return; }
+
+	int nListIndex = m_ctrlListControlProxy.GetItemCount();
+
+	m_ctrlListControlProxy.InsertItem(nListIndex, _Device);
+
+	m_ctrlListControlProxy.SetItemText(nListIndex, 1, _ip);
+	m_ctrlListControlProxy.SetItemText(nListIndex, 2, _Ethernet);
+	m_ctrlListControlProxy.SetColumnWidth(1, LVSCW_AUTOSIZE_USEHEADER);
+	m_ctrlListControlProxy.SendMessage(WM_VSCROLL, SB_BOTTOM);	// Scroll to bottom
+}
+
+void CARPDlg::OnBnClickedButtonAdd()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	mDeviceAddDlg.InitDeviceAddDlg(0);
+	mDeviceAddDlg.ShowWindow(SW_SHOW);
+}
+
+
+void CARPDlg::OnBnClickedButtonDelete()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	POSITION pos = m_ctrlListControlProxy.GetFirstSelectedItemPosition();
+	int nItem = m_ctrlListControlProxy.GetNextSelectedItem(pos);
+	m_ctrlListControlProxy.DeleteItem(nItem);
+}
+
+
+void CARPDlg::OnBnClickedButtonItemDel()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	POSITION pos = m_ctrlListControl.GetFirstSelectedItemPosition();
+	int nIndex = m_ctrlListControl.GetNextSelectedItem(pos);
+	if (nIndex > -1)
+	{
+		m_ctrlListControl.DeleteItem(nIndex);
+	}
+}
+
+
+void CARPDlg::OnBnClickedButtonAllDel()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	m_ctrlListControl.DeleteAllItems();
+}
